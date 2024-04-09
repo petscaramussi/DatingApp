3532731c9002693f161db7757731ade781b9af44
Peterson Scaramussi <peterson.silva6@fatec.sp.gov.br>
2024-04-09 11:30:41 -0300

    configure cors and connect to api

--- API/Program.cs
+++ API/Program.cs
@@ -11,11 +11,16 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Configure the HTTP request pipeline
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
