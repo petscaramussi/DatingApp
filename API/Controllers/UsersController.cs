0518a570416e5eed6d2910d2f9fc70bf8d2068f4
Peterson <peterson.silva6@fatec.sp.gov.br>
2024-06-04 12:00:00 -0300

    Extending the API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -25,19 +25,15 @@ public class UsersController : BaseApiController
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = await _userRepository.GetUsersAsync();
+        var users = await _userRepository.GetMembersAsync();
 
-        var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
-
-        return Ok(usersToReturn);
+        return Ok(users);
     }
 
     [HttpGet("{username}")] // /api/users/2
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await _userRepository.GetUserByUserNameAsync(username);
-
-        return _mapper.Map<MemberDto>(user);
+        return await _userRepository.GetMemberAsync(username);
     }
 
 }
--- API/Interfaces/IUserRepository.cs
+++ API/Interfaces/IUserRepository.cs
@@ -1,3 +1,4 @@
+using API.DTOs;
 using API.Entities;
 
 namespace API.Interfaces
@@ -8,6 +9,8 @@ namespace API.Interfaces
         Task<bool> SaveAllAsync();
         Task<IEnumerable<AppUser>> GetUsersAsync();
         Task<AppUser> GetUserByIdAsync(int id);
-        Task<AppUser> GetUserByUserNameAsync(string username);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<MemberDto> GetMemberAsync(string username);
     }
 }
